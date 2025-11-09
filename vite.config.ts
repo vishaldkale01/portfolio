615de2bada8698c910472dcdfc4077533807e255
vishal.kale <vishaldkale0@gmail.com>
2025-11-09 22:53:27 +0530

    Remove GitHub Actions workflow for static deployment, update package-lock.json to include gh-pages and related dependencies, and modify package.json to set homepage. Adjust Vite configuration to correct API target URL and maintain base path for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/', // ✅ Must match your repo name
   server: {
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://portfolio-etg0.onrender.com/api',
+        target: 'https://portfolio-etg0.onrender.com',
         changeOrigin: true,
       },
     },
-    allowedHosts: ['.vercel.app', '.github.io' , '.ngrok-free.app'],
+    allowedHosts: ['.vercel.app', '.github.io', '.ngrok-free.app'],
   },
-}
-);
+});
